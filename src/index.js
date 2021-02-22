868a3c493d521b6f5206850803cdf5406833bbb2
songseop.lee <lss3070@hanmail.net>
2021-02-22 23:01:32 +0900

    init setting

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import reportWebVitals from './reportWebVitals';
+
+import {ApolloProvider} from "@apollo/react-hooks"
+import client from "./apollo";
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
